784fd500ca9cdf7e92afd1bc022cfd5d5fcbbee1
Hack Reactor Students <github@hackreactor.com>
2013-10-08 20:38:33 -0700

    minor changes

--- client/scripts/backbone-app.js
+++ client/scripts/backbone-app.js
@@ -1,95 +1,53 @@
 
-
-
-var User = Backbone.Model.extend({
-  url: 'https://api.parse.com/1/classes/chatterbox'
-
-});
-
-var Users = Backbone.Collection.extend({
-
-});
-
-// var Message = Backbone.Model.extend({
-//   url: 'https://api.parse.com/1/classes/chatterbox'
-// });
-
 var Messages = Backbone.Collection.extend({
   url: 'https://api.parse.com/1/classes/chatterbox'
+
 });
 
 var MessageListView = Backbone.View.extend({
+
+  initialize : function () {
+    if(this.collection){
+    this.collection.on('change', this.render, this);
+    }
+  },
+
   el : $(".chatBox"),
+
+  currentRoom : $('.roomInput').val() || 'lobby',
+
   render : function () {
+
     var that = this;
-    console.log(that);
     var messages = new Messages();
+    console.log('room is',that.currentRoom);
     messages.fetch({
       success: function(messages){
-        console.log('success!',messages.models[0].get('results'));
+        // console.log('success!',messages.models[0].get('results'));
         var template = _.template($('#message-template').html(), {messages: messages.models[0].get('results')}); // messages.models
-        // console.log('template',template);  
         that.$el.html(template);
+      },
+      data : {
+      'order': '-createdAt',
+      'limit': 20,
+      'roomname':that.currentRoom
       }
     });
+  },
+
+  events : {
+    'submit .roomsChange': 'changeRoom'
+  },
+
+  changeRoom : function (e) {
+    e.preventDefault();
+    console.log('rendered');
+    this.render();
+
   }
 });
 
 
 
 Backbone.history.start();
-
-
-
-
-
-
-
-// <td><%=thing['username']%></td>
-//               <td><%=thing.text%></td>
-//               <td><%=thing.roomname%></td>
-
-
-
-
-
-
-
-
-// var getMessages = function(){
-//   $.ajax({
-    
-//     type: 'GET',
-//     contentType: 'application/json',
-//     data: {
-//      
-//     },
-//     success: function(data) {
-//       console.log(data.results);
-//     },
-//     error: function (data) {
-//       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//       console.error('chatterbox: Failed to receive message');
-//     }
-//   });
-// };
-
-
-
-
-// var postMessage = function (msg,name,currentRoom) {
-//   $.ajax({
-//     url: 'https://api.parse.com/1/classes/chatterbox',
-//     type: 'POST',
-//     data: JSON.stringify(message(msg,name,currentRoom)),
-//     contentType: 'application/json',
-//     success: function (data) {
-//       console.log('chatterbox: Message sent');
-//     },
-//     error: function (data) {
-//       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//       console.error('chatterbox: Failed to send message');
-//     }
-//   });
-// };
 
