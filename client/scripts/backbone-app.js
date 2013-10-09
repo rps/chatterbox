676259f1ea077df2e80e131bbc8ecf0b3be9cd30
Rich Parrish <tradename@gmail.com>
2013-10-08 23:14:47 -0700

    supports posting and nearly automated updates.

--- client/scripts/backbone-app.js
+++ client/scripts/backbone-app.js
@@ -1,29 +1,51 @@
+$.fn.serializeObject = function() {
+  var o = {};
+  var a = this.serializeArray();
+  console.log(a);
+  $.each(a, function() {
+      if (o[this.name] !== undefined) {
+          if (!o[this.name].push) {
+              o[this.name] = [o[this.name]];
+          }
+          o[this.name].push(this.value || '');
+      } else {
+          o[this.name] = this.value || '';
+      }
+  });
+  return o;
+};
+
+
+var Message = Backbone.Model.extend({
+  url: 'https://api.parse.com/1/classes/chatterbox'
+});
 
 var Messages = Backbone.Collection.extend({
-  url: 'https://api.parse.com/1/classes/chatterbox'
-
+  model: Message,
+  url: 'https://api.parse.com/1/classes/chatterbox',
+     initialize : function(){
+        console.log('tagfeed model is initialized');
+        this.on("change", function(){
+            console.log("An attribute has been changed");
+        });
+    }
 });
 
 var MessageListView = Backbone.View.extend({
 
   initialize : function () {
-    if(this.collection){
-    this.collection.on('change', this.render, this);
-    }
+    this.listenTo(this.model, 'change', this.render);
   },
 
   el : $(".chatBox"),
 
-  currentRoom : $('.roomInput').val() || 'lobby',
+  currentRoom : 'lobby',
 
   render : function () {
 
     var that = this;
-    var messages = new Messages();
-    console.log('room is',that.currentRoom);
-    messages.fetch({
-      success: function(messages){
-        // console.log('success!',messages.models[0].get('results'));
+    this.model.fetch({
+      success: function(messages){         
         var template = _.template($('#message-template').html(), {messages: messages.models[0].get('results')}); // messages.models
         that.$el.html(template);
       },
@@ -36,18 +58,29 @@ var MessageListView = Backbone.View.extend({
   },
 
   events : {
-    'submit .roomsChange': 'changeRoom'
+    'click .changeRoom': 'changeRoom',
+    'click .sendMsg': 'postText',
   },
 
-  changeRoom : function (e) {
-    e.preventDefault();
-    console.log('rendered');
+  changeRoom : function () {
+    this.currentRoom = $('.roomInput').val();
     this.render();
+  },
 
+  postText : function () {
+    this.model.create({
+      'roomname':this.currentRoom,
+      'text':$('.chatInput').val(),
+      'username':window.location.search.slice(window.location.search.indexOf('=')+1)
+    });
+  }
+
+});
+
+var Router = Backbone.Router.extend({
+  routes: {
+    '':'home'
   }
 });
 
-
-
-Backbone.history.start();
-
+// Backbone.history.start();
